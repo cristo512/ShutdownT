91ebcbc5e6517f021b03f51d3c7c3255352fa28a
cristo512 <cristo512@o2.pl>
2014-06-12 21:52:47 +0200

    sda
    asd

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <windows.h>
@@ -6,7 +6,9 @@
 #include <sstream>
 
 sf::Sprite field[9],quit_sprite;
-int delay[6];
+sf::RectangleShape prostokat(sf::Vector2f(405,50));
+int delay[8];
+std::string cokolwiek;
 
 void setShutdown(int currentTime)
 {
@@ -20,12 +22,14 @@ void setShutdown(int currentTime)
     system(temp);
 }
 
-int checkEvent(sf::Event &event, sf::Mouse &mouse, sf::RenderWindow &window)
+void checkEvent(sf::Event &event, sf::RenderWindow &window, sf::Mouse &mouse)
 {
     if(event.type==sf::Event::Closed)
         window.close();
-    for(int i=0;i<7;i++)
+    for(int i=0;i<9;i++)
     {
+        if(i==7)
+            continue;
         if(mouse.getPosition(window).x>150+(i%3)*135
         &&mouse.getPosition(window).x<150+(i%3)*135+field[i].getGlobalBounds().width
         &&mouse.getPosition(window).y>150+(i/3)*112
@@ -67,48 +71,64 @@ int checkEvent(sf::Event &event, sf::Mouse &mouse, sf::RenderWindow &window)
     }
     else
         quit_sprite.setColor(sf::Color::White);
+
+    if(event.type == sf::Event::KeyPressed)
+    {
+        if(event.key.code >= sf::Keyboard::Num0 && event.key.code <= sf::Keyboard::Num9 )
+        {
+            std::cout<< 'A' + char(event.key.code)<<std::endl;
+        }
+
+        else if(event.key.code == sf::Keyboard::BackSpace )
+        {
+            std::cout<< 'A' + char(event.key.code)<<std::endl;
+        }
+
+        else if(event.key.code == sf::Keyboard::Return)
+        {
+            window.close();
+        }
+    }
 }
 
 
 int main()
 {
-    std::string sdown[7];
     delay[0] = 1800;
     delay[1] = 2400;
     delay[2] = 3000;
     delay[3] = 3600;
     delay[4] = 7200;
     delay[5] = 14400;
-    delay[6] = 36000;
+    delay[6] = 18000;
+    delay[8] = 36000;
 
     sf::RenderWindow window(sf::VideoMode(800,600), "Shutdown");
     sf::Texture shutdown_texture;
     if(!shutdown_texture.loadFromFile("blok.png"))
        return -1;
-    sf::Sprite field[9],quit_sprite;
     sf::Font czcionka;
     if(!czcionka.loadFromFile("C:\\Windows\\Fonts\\arial.ttf"))
         return -2;
-    sf::Text s[8],q;
-    s[0].setString("30min");
-    s[1].setString("40min");
-    s[2].setString("50min");
+    sf::Text s[9],q;
+    s[0].setString("30 min");
+    s[1].setString("40 min");
+    s[2].setString("50 min");
     s[3].setString("1 h");
     s[4].setString("2 h");
     s[5].setString("4 h");
-    s[6].setString("10 h");
-    s[7].setString("Wylacz");
-    q.setString("Wyjdz");
+    s[6].setString("5 h");
+    s[7].setString(L"Wyłącz");
+    s[8].setString("10 h");
+    q.setString(L"Wyjdź");
 
     for (int i=0;i<9;i++)
     {
         field[i].setTexture(shutdown_texture);
-        field[i].setPosition(150+(i%3)*140 , 150+(i/3)*117);
-        if(i==8)
-            break;
+        field[i].setPosition(150+(i%3)*140 , 200+(i/3)*117);
         s[i].setFont(czcionka);
         s[i].setPosition( field[i].getPosition().x+ (field[i].getGlobalBounds().width - s[i].getGlobalBounds().width)/2 ,
-                         field[i].getPosition().y+ (field[i].getGlobalBounds().height - s[i].getGlobalBounds().height)/2 );
+                        field[i].getPosition().y+ (field[i].getGlobalBounds().height - s[i].getGlobalBounds().height)/2 );
         s[i].setColor(sf::Color::Blue);
     }
     quit_sprite.setTexture(shutdown_texture);
@@ -117,7 +137,7 @@ int main()
     q.setFont(czcionka);
     q.setPosition(quit_sprite.getPosition().x+(quit_sprite.getGlobalBounds().width-q.getGlobalBounds().width)/2 ,
                   quit_sprite.getPosition().y+(quit_sprite.getGlobalBounds().height-q.getGlobalBounds().height)/2);
-
+    prostokat.setPosition(150, 30);
     sf::Event event;
     sf::Mouse mouse;
     window.setFramerateLimit(60);
@@ -126,8 +146,9 @@ int main()
         window.clear();
         while(window.pollEvent(event))
         {
-            checkEvent(event, mouse, window);
+            checkEvent(event, window, mouse);
         }
+        window.draw(prostokat);
         for(int i=0;i<9;i++)
         {
             window.draw(field[i]);
